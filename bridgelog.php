947037cd37a306a9bb18ac4557b21a2036ce0148
fajar diki <fajarsiddiqi@gmail.com>
2020-02-26 14:49:50 +0700

    Update bridgelog.php

--- bridgelog.php
+++ bridgelog.php
@@ -2,8 +2,11 @@
 
 	include "conn.php";
 
+	// place this before any script you want to calculate time
+    $time_start = microtime(true);
+
 	// AUTH TENANT
-	$bridge_log = "SELECT * FROM bridge_log LIMIT 100000";
+	$bridge_log = "SELECT * FROM bridge_log LIMIT 1000000";
 	$query_bridge_log = mysqli_query($koneksi,$bridge_log);
 
 	while ($dtat=mysqli_fetch_array($query_bridge_log)) {
@@ -19,9 +22,16 @@
 		);
 	}
 
+	$time_end = microtime(true);
+
+	$execution_time = round(($time_end - $time_start), 3);
+
+	$result = number_format($execution_time,3);
+
 	$json = array(
 		'result'=>'succes',
-		'Bridge_Log'=>$item
+		'Bridge_Log'=>$item,
+		'Time'=>$execution_time
 	);
 
 	echo json_encode($json);
